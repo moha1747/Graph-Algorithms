cdbaeb057788f21e7a6f82f99a8a9593a9c2e4ef
moha1747 <mohamud.abbaas14@gmail.com>
2024-07-23 06:36:28 -0500

    Last Algo's Implementation: {Dijkstra, BidirectionalSearch}

---
+++ src/Algorithms/BidirectionalSearch.js
@@ -0,0 +1,98 @@
+// Bidirectional Breadth First Search
+export function BidirectionalSearch(startNode, goalNode) {
+  var path = [];
+  var pathFoundMessage = "CANT FIND PATH";
+  var pathToGoal = [];
+  var pathIsFound = false;
+  var algoName = "Bidirectional Search"
+
+  var sourceQueue = [];
+  var destinationQueue = [];
+  sourceQueue.push(startNode);
+  destinationQueue.push(goalNode);
+
+
+  while (sourceQueue.length !== 0 || destinationQueue.length !== 0) {
+    if (sourceQueue.length !== 0) {
+      let currentNodeFwd = sourceQueue.shift();
+      path.push(currentNodeFwd);
+      let adjacentNodesFwd = currentNodeFwd.adjacentNodes;
+      
+      currentNodeFwd.isVisited = true;
+
+      if (currentNodeFwd.isVisited && currentNodeFwd.isVisitedBidirectional) {
+        pathFoundMessage = `PATH FOUND - INTERSECTION (FORWARD): ${currentNodeFwd.row} ${currentNodeFwd.col}`;
+
+        pathIsFound = true;
+
+        let tempNodeFwd = currentNodeFwd;
+        pathToGoal.push(tempNodeFwd);
+        while (tempNodeFwd.previousNode) {
+          pathToGoal.push(tempNodeFwd.previousNode);
+          tempNodeFwd = tempNodeFwd.previousNode;
+        }
+  
+        pathToGoal = pathToGoal.reverse();
+
+        let tempNodeBack = currentNodeFwd;
+        while (tempNodeBack.previousNodeBidirectional) {
+          pathToGoal.push(tempNodeBack.previousNodeBidirectional);
+          tempNodeBack = tempNodeBack.previousNodeBidirectional;
+        }
+
+        return { path, pathFoundMessage, pathToGoal, pathIsFound, algoName };
+
+      }
+
+      for (let i = 0; i < adjacentNodesFwd.length; i++) {
+        if (!adjacentNodesFwd[i].isVisited) {
+          sourceQueue.push(adjacentNodesFwd[i]);
+          adjacentNodesFwd[i].isVisited = true;
+          adjacentNodesFwd[i].previousNode = currentNodeFwd;
+        }
+      }
+    }
+    if (destinationQueue.length !== 0) {
+      let currentNodeBack = destinationQueue.shift();
+      path.push(currentNodeBack);
+      let adjacentNodesBack = currentNodeBack.adjacentNodes;
+      // console.log(`VISITED ${currentNodeBack.row} ${currentNodeBack.col}`);
+      currentNodeBack.isVisitedBidirectional = true;
+
+      if (currentNodeBack.isVisited && currentNodeBack.isVisitedBidirectional) {
+        pathFoundMessage = `PATH FOUND - INTERSECTION (BACK): ${currentNodeBack.row} ${currentNodeBack.col}`;
+
+        pathIsFound = true;
+
+        let tempNodeBack = currentNodeBack;
+        pathToGoal.push(tempNodeBack);
+        while (tempNodeBack.previousNodeBidirectional) {
+          pathToGoal.push(tempNodeBack.previousNodeBidirectional);
+          tempNodeBack = tempNodeBack.previousNodeBidirectional;
+        }
+  
+        pathToGoal = pathToGoal.reverse();
+
+        let tempNodeFwd = currentNodeBack;
+        while (tempNodeFwd.previousNode) {
+          pathToGoal.push(tempNodeFwd.previousNode);
+          tempNodeFwd = tempNodeFwd.previousNode;
+        }
+
+        pathToGoal = pathToGoal.reverse();
+        
+        return { path, pathFoundMessage, pathToGoal, pathIsFound, algoName };
+      }
+
+      for (let i = 0; i < adjacentNodesBack.length; i++) {
+        if (!adjacentNodesBack[i].isVisitedBidirectional) {
+          destinationQueue.push(adjacentNodesBack[i]);
+          adjacentNodesBack[i].isVisitedBidirectional = true;
+          adjacentNodesBack[i].previousNodeBidirectional = currentNodeBack;
+        }
+      }
+    }
+  }
+
+  return { path, pathFoundMessage, pathIsFound, algoName };
+}
---
+++ src/Algorithms/Dijkstra.js
@@ -0,0 +1,52 @@
+import { PriorityQueue } from "./PriorityQueue";
+
+// Dijkstra Algorithm (No indexed priority queue)
+export function Dijkstra(startNode, goalNode) {
+  var path = [];
+  var pathFoundMessage = "CANT FIND PATH";
+  var pathToGoal = [];
+  var pathIsFound = false;
+  var algoName = "Dijkstra's Algorithm"
+
+  var pq = new PriorityQueue();
+
+  startNode.distance = 0;
+  pq.enqueue([startNode, 0]);
+
+  while (!pq.isEmpty()) {
+    let currentNode = pq.dequeue();
+    currentNode.isVisited = true;
+    path.push(currentNode);
+
+    if (currentNode === goalNode) {
+      // console.log(`PATH FOUND ${currentNode.row} ${currentNode.col}`);
+      pathFoundMessage = `PATH FOUND ${currentNode.row} ${currentNode.col}`;
+
+      pathIsFound = true;
+
+      let tempNode = currentNode;
+      pathToGoal.push(tempNode);
+      while (tempNode.previousNode) {
+        pathToGoal.push(tempNode.previousNode);
+        tempNode = tempNode.previousNode;
+      }
+
+      pathToGoal = pathToGoal.reverse();
+      return { path, pathFoundMessage, pathToGoal, pathIsFound, algoName };
+    }
+
+    let adjacentNodes = currentNode.adjacentNodes;
+    for (let i = 0; i < adjacentNodes.length; i++) {
+      if (!adjacentNodes[i].isVisited) {
+        let newDist = currentNode.distance + 1;
+        if (newDist < adjacentNodes[i].distance) {
+          adjacentNodes[i].distance = newDist;
+          pq.enqueue([adjacentNodes[i], adjacentNodes[i].distance]);
+          adjacentNodes[i].previousNode = currentNode;
+        }
+      }
+    }
+  }
+
+  return { path, pathFoundMessage, pathIsFound, algoName };
+}
